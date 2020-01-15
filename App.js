6ca37984ba12f7b2779eecadc380f54e2cade551
Cyrus Chan <cyruschan@hsu.edu.hk>
2020-01-15 15:47:27 +0800

    workable in both android and ios

--- App.js
+++ App.js
@@ -3,7 +3,6 @@ import { StyleSheet, Text, View, Button } from "react-native";
 import { WebView } from "react-native-webview";
 
 export default function App() {
-
   const injectedJavascript = `(function() {
     window.WebViewBridge = {
       onMessage: function() {
@@ -16,33 +15,39 @@ export default function App() {
     var event = new Event('WebViewBridge');
     window.dispatchEvent(event);
   })()`;
-  
+
   const generateOnMessageFunction = data =>
     `(function() {
       window.WebViewBridge.onMessage(${JSON.stringify(data)});
     })()`;
-  
-  const sendMessageToWebView = (message) => {
+
+  const sendMessageToWebView = message => {
     WebViewRef.injectJavaScript(generateOnMessageFunction(message));
-    };
-    const sendMessageToWeb = ()=>{
-     WebViewRef.postMessage('some message')
+  };
+  const sendMessageToWeb = () => {
+    const clientResponseCode = `
+      window.postMessage(${JSON.stringify('dsafasdf')}, "*");
+      true;
+    `;
+     WebViewRef.injectJavaScript(clientResponseCode);
+    if (WebViewRef) {
+      WebViewRef.injectJavaScript(clientResponseCode);
     }
-setTimeout(()=>{
-  // window.ReactNativeWebView = window['ReactAPI36_0_0NativeWebView'];
-  if(WebViewRef){
-    alert('posting')
-    // sendMessageToWebView('send message')
-    // WebViewRef.postMessage('data');
-   
-      
-    
-    WebViewRef.postMessage('red')
-  }else{
-    WebViewRef.postMessage('red')
-    alert('webview not exits')
-  }
-},3000)
+    // WebViewRef.postMessage("some message");
+  };
+  setTimeout(() => {
+    // window.ReactNativeWebView = window['ReactAPI36_0_0NativeWebView'];
+    if (WebViewRef) {
+      alert("posting");
+      // sendMessageToWebView('send message')
+      // WebViewRef.postMessage('data');
+
+      WebViewRef.postMessage("red");
+    } else {
+      WebViewRef.postMessage("red");
+      alert("webview not exits");
+    }
+  }, 3000);
   // const postMessage = ()=>{
   //   window.ReactNativeWebView.postMessage('data');
   // }
@@ -58,25 +63,29 @@ setTimeout(()=>{
     window.ReactNativeWebView.postMessage(JSON.stringify(window.location));
 })();`;
   return (
-    <View style={{flex:1}}>
-      <Button title="no function" style={{paddingTop:100,marginTop:100}}/>
-      <Button title="enter" onPress={sendMessageToWeb} style={{paddingTop:399,marginTop:300}}/>
-    <WebView
-    useWebKit={true}
-    ref={WEBVIEW_REF => (WebViewRef = WEBVIEW_REF)}
-    javaScriptEnabled={true}
-    // injectedJavaScript={INJECTED_JAVASCRIPT}
-    // source={{ html }}
-    // onMessage={event => {
-    //   alert(event.nativeEvent.data);
-    // }}
-    onLoadEnd={()=>WebViewRef.postMessage('red')}
-      source={{
-        uri:
-          "https://ecampus-dev.hsu.edu.hk/moodle/webview_new_testing_web/index2.html"
-      }}
-      style={{ marginTop: 20 }}
-    />
+    <View style={{ flex: 1 }}>
+      <Button title="no function" style={{ paddingTop: 100, marginTop: 100 }} />
+      <Button
+        title="enter"
+        onPress={sendMessageToWeb}
+        style={{ paddingTop: 399, marginTop: 300 }}
+      />
+      <WebView
+        useWebKit={true}
+        ref={WEBVIEW_REF => (WebViewRef = WEBVIEW_REF)}
+        javaScriptEnabled={true}
+        // injectedJavaScript={INJECTED_JAVASCRIPT}
+        // source={{ html }}
+        // onMessage={event => {
+        //   alert(event.nativeEvent.data);
+        // }}
+        onLoadEnd={() => WebViewRef.postMessage("red")}
+        source={{
+          uri:
+            "https://ecampus-dev.hsu.edu.hk/moodle/webview_new_testing_web/index.html"
+        }}
+        style={{ marginTop: 20 }}
+      />
     </View>
   );
 }
